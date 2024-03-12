5b7b29f33c70bfbca62fa47ffb77d1db1ed0ef4c
Kaleem Bhatti <kaleembhatti.work@gmail.com>
2024-03-12 22:21:18 +0500

    Prod: Fixed hostname

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
 		remotePatterns: [
 			{
 				protocol: 'https',
-				hostname: 'kaleembhattiworks.com',
+				hostname: 'digitalhippo.kaleembhattiworks.com',
 				port: '',
 				pathname: '/media/**',
 			},
